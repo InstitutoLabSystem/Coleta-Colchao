fb856ba9b8db7a22ca76edc0febc725b142313b8
LabsystemDev1 <137182284+LabsystemDev1@users.noreply.github.com>
2023-10-23 08:46:04 -0300

    Primeiro git

---
+++ Coleta-Colchao/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Coleta_Colchao.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
