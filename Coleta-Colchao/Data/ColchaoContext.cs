620d1a8610b90acab0474ca5ed26013d456afe96
LabsystemDev1 <137182284+LabsystemDev1@users.noreply.github.com>
2023-11-07 16:40:31 -0300

    feature: criando a forma de escolher o ensaio.

---
+++ Coleta-Colchao/Data/ColchaoContext.cs
@@ -0,0 +1,14 @@
+﻿using Coleta_Colchao.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Coleta_Colchao.Data
+{
+    public class ColchaoContext : DbContext
+    {
+        public ColchaoContext(DbContextOptions<ColchaoContext> options) : base(options)
+        {
+        }
+
+    }
+
+}
