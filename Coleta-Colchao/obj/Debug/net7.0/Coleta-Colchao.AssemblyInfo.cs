86096a8a96a3c3b89accb6c23a44c3c5b27a83a7
LabsystemDev1 <137182284+LabsystemDev1@users.noreply.github.com>
2023-11-22 08:18:28 -0300

    fix: correção de conflito na model

--- Coleta-Colchao/obj/Debug/net7.0/Coleta-Colchao.AssemblyInfo.cs
+++ Coleta-Colchao/obj/Debug/net7.0/Coleta-Colchao.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Coleta-Colchao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5ad472b46d108f676375566ed3b5ddf5d4d4137")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70d5325ec29206cc5e9cd292e076b366e1b08197")]
 [assembly: System.Reflection.AssemblyProductAttribute("Coleta-Colchao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Coleta-Colchao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
