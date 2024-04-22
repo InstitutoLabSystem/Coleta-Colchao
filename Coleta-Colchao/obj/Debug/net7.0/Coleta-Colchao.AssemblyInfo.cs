c68422af943f895563d6b812eb25ff2d08836a1e
LabsystemDev1 <137182284+LabsystemDev1@users.noreply.github.com>
2024-04-22 15:23:24 -0300

    subindo gitignore,

--- Coleta-Colchao/obj/Debug/net7.0/Coleta-Colchao.AssemblyInfo.cs
+++ Coleta-Colchao/obj/Debug/net7.0/Coleta-Colchao.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Coleta-Colchao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1d914a4e0fd51eab466009c08b732befacaa849")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5c2de8248422fb0ed8d809e89c4984e22fb237f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Coleta-Colchao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Coleta-Colchao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
