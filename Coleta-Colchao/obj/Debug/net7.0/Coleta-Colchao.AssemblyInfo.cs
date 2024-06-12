0c48a1e2bb2a1ab00a3523625c5ad54863316ef2
Gabriel dos Santos Silva <dev3@labsystem.com.br>
2024-06-12 13:37:41 -0300

    docs:update
    atualizando coleta na branch Gabriel

--- Coleta-Colchao/obj/Debug/net7.0/Coleta-Colchao.AssemblyInfo.cs
+++ Coleta-Colchao/obj/Debug/net7.0/Coleta-Colchao.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Coleta-Colchao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c68422af943f895563d6b812eb25ff2d08836a1e")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("Coleta-Colchao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Coleta-Colchao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
