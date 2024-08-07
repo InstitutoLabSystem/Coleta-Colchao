d1313740a1f2563749401dea4d97fd48a368e44f
LabsystemDev1 <137182284+LabsystemDev1@users.noreply.github.com>
2024-08-07 14:44:59 -0300

    att de deploy

--- Coleta-Colchao/obj/Release/net7.0/Coleta-Colchao.AssemblyInfo.cs
+++ Coleta-Colchao/obj/Release/net7.0/Coleta-Colchao.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Coleta-Colchao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51a786161d8d76e3faa9e274fc949694f9b0b709")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0736630b7b7a92407b8ea4eee0f9fc4aa30877b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Coleta-Colchao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Coleta-Colchao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
