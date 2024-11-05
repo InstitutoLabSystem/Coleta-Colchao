406a5dd821862499b2f3f4173f69a98146b3a8dc
LabsystemDev1 <137182284+LabsystemDev1@users.noreply.github.com>
2024-11-05 15:29:58 -0300

    fix: correção de porcentagem errado.

--- Coleta-Colchao/obj/Debug/net7.0/Coleta-Colchao.AssemblyInfo.cs
+++ Coleta-Colchao/obj/Debug/net7.0/Coleta-Colchao.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Coleta-Colchao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e8a324356dd76f99ce1d4360b064cf1bdaf6ad7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec263315d997175a742ba5674b22ee8cbcd3367f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Coleta-Colchao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Coleta-Colchao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
