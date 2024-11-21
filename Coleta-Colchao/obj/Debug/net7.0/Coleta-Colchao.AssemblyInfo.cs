c9360c5255cd70a78c4e8850aac1b084d7de081a
LabsystemDev1 <137182284+LabsystemDev1@users.noreply.github.com>
2024-11-21 12:18:15 -0300

    fix: correção da data que nao estava mostrando dps de salvar.

--- Coleta-Colchao/obj/Debug/net7.0/Coleta-Colchao.AssemblyInfo.cs
+++ Coleta-Colchao/obj/Debug/net7.0/Coleta-Colchao.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Coleta-Colchao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b11f18541d273034ba1a432679bb68681bfdb6c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fe6be1ec8272bbfe2eefa1a3197ae195a3f89b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Coleta-Colchao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Coleta-Colchao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
