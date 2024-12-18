eb50407c174f4c96f6c67b9eff0ceb7bec793950
LabsystemDev1 <137182284+LabsystemDev1@users.noreply.github.com>
2024-12-18 16:11:03 -0300

    feature: incluindo --- na parte do index espuma referente o tipo de espuma.

--- Coleta-Colchao/obj/Debug/net7.0/Coleta-Colchao.AssemblyInfo.cs
+++ Coleta-Colchao/obj/Debug/net7.0/Coleta-Colchao.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Coleta-Colchao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fefa473f6f876af6f3801ceafdcc61c80ae4ed0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+746fce5d972c95085b7581609b928fac7e88bed5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Coleta-Colchao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Coleta-Colchao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
