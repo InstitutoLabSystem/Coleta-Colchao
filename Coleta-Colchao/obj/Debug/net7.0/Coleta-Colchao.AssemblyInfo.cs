96f0354212cad0b407d71c1776dfb0321f929f5e
LabsystemDev1 <137182284+LabsystemDev1@users.noreply.github.com>
2025-02-05 15:56:10 -0300

    update: atualizado footer para não ser flutuante

--- Coleta-Colchao/obj/Debug/net7.0/Coleta-Colchao.AssemblyInfo.cs
+++ Coleta-Colchao/obj/Debug/net7.0/Coleta-Colchao.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Coleta-Colchao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a14262e672b131fb35e490c4b835f8901981e8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adaff96f4edb101d053f7e80ee452adf4dbca692")]
 [assembly: System.Reflection.AssemblyProductAttribute("Coleta-Colchao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Coleta-Colchao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
